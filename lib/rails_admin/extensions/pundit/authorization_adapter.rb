b817c6093ca65ac5b492cba5fa0e5812d67ab4ab
Jesse Sanford <jessesanford@gmail.com>
2014-10-25 16:59:38 -0700

    added check for record

--- lib/rails_admin/extensions/pundit/authorization_adapter.rb
+++ lib/rails_admin/extensions/pundit/authorization_adapter.rb
@@ -29,7 +29,9 @@ module RailsAdmin
         # instance if it is available.
         def authorize(action, abstract_model = nil, model_object = nil)
           record = model_object || abstract_model && abstract_model.model
-          @controller.authorize record
+          if record
+            @controller.authorize record
+          end
           unless policy(record).rails_admin?(action)
             raise ::Pundit::NotAuthorizedError, "not allowed to #{action} this #{record}"
           end
